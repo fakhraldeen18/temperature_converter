723ed5516b3c4df261cc5f9d9f51fc90e5a38c56
fakhraldeen18 <fakhraldeen18@gmail.com>
2024-03-18 11:23:37 +0300

    Added a temperatu conversions feture

--- src/Program.cs
+++ src/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Xml.XPath;
+
+var temperature = new Temperature();
+
+Console.WriteLine(temperature.TempConvert("F",32)); 
+
+class Temperature (){
+
+
+
+    public string TempConvert(string unit, int amount){
+        if(unit == "F"){
+           int result =  (amount - 32) * 5/9;
+            return result.ToString() + " C";
+        }
+        else if(unit == "C"){
+            int result = (amount * 9/5) + 32;
+            return result.ToString() + " F";
+
+        }
+        else{
+            return "invalid temperature";
+        }
+    }
+}
+
